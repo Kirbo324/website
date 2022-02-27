6379665e91a53f2f7f4492b2879d5c0c636e9d09
thrizzle <thrizzle@terabyteis.me>
2022-02-27 17:06:33 -0500

    Updates

--- _next/static/OGc-NxlS4FQMQGRn7S_06/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/622-13fff22f8c2ebae7.js","static/chunks/pages/index-486e41f9ed57bfa8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
